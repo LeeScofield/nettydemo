684f71aa9dc85c9b312ffe36d155023ac2a43fe4
LeeScofield <175525316@qq.com>
2019-07-18 23:43:39 +0800

    添加netty第一个例子

--- nettydemo/src/main/java/netty/demo/TestHttpServerHandler.java
+++ nettydemo/src/main/java/netty/demo/TestHttpServerHandler.java
@@ -9,6 +9,8 @@ import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
 
+import java.net.URI;
+
 /**
  * @author Lee
  * date:2019-07-16
@@ -18,8 +20,17 @@ public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObjec
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, HttpObject httpObject) throws Exception {
         if (httpObject instanceof HttpRequest) {
-            ByteBuf content = Unpooled.copiedBuffer("hello world", CharsetUtil.UTF_8);
+            HttpRequest httpRequest = (HttpRequest)httpObject;
 
+            System.out.println("请求方法名:" + httpRequest.method().name());
+
+            URI uri = new URI(httpRequest.uri());
+            if ("/favicon.ico".equals(uri.getPath())) {
+                System.out.println("请求favicon.ico");
+                return;
+            }
+
+            ByteBuf content = Unpooled.copiedBuffer("hello world", CharsetUtil.UTF_8);
             FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
 
             response.headers().add(HttpHeaderNames.CONTENT_TYPE, "text/plain");
@@ -27,5 +38,35 @@ public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObjec
 
             channelHandlerContext.writeAndFlush(response);
         }
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelActive");
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelInactive");
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handlerAdded");
+        super.handlerAdded(ctx);
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelRegistered");
+        super.channelRegistered(ctx);
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelUnregistered");
+        super.channelUnregistered(ctx);
     }
 }
